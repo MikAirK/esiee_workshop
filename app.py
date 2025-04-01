11c7738b6a03eda6eff47637c2510f51d440e4cf
MikRK <michael.kisuka94@gmail.com>
2025-04-01 20:25:36 +0200

    fonction who return True or False

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+"""
 print("Hello")
 print("")
 print("TDB")
+"""
+#TASK Write an IF ELSE statement to validate if x is larger than y. Return TRUE if YES
+
+
+
+
+def superiorornot ():
+    x = int(input("Entre x : "))
+    y = int(input("Entre y : "))
+    
+    
+    if x > y :
+        return True
+    else:
+        return False
+
+print(superiorornot())
